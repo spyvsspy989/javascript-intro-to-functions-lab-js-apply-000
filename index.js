d8e54b27d21b3111632304e9f1b20c6887a1abe0
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-07-11 00:03:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function adds(){
+  var result = []; // Create a new Array
+  var i = [i = "0", j = "1", k = "2", l = "2"];
+  [i = "0", j = "1", k = "2", l = "3", m = "4", n = "5", "...."];
+  
+function adds() {
+return i + "1";
+return j + "1";
+return k + "1";
+return l + "1";
+return m + "1";
+return "z" + "1";
+
+
+String = ["0","1", "2", "3", "4", "5", "6", "..."];
+console.log(i + "1");
